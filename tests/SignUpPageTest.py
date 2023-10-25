f984467c0cf505ef33ec50c9af58ed7fe78c43b3
ZYP <2359598489@qq.com>
2023-10-25 15:09:10 +0800

    finish login part

--- tests/SignUpPageTest.py
+++ tests/SignUpPageTest.py
@@ -1,9 +1,11 @@
 import json
+import os
+import platform
 import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from components import button_click, input_box, box_select, date_picker, upload_file
+from components import button_click, input_box, box_select, date_picker, upload_file, download_file
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options as EdgeOption
 
@@ -15,11 +17,12 @@ password = "123456"
 
 options = EdgeOption()
 # driver = webdriver.Edge(options=options)
+options.platform_name = 'macOS 13'
 sauce_options = {}
-sauce_options['username'] = 'AUV-ZYP'
-sauce_options['accessKey'] = '093f748f-5860-443a-b183-e9cb1d9d0965'
-sauce_options['build'] = 'selenium-build-U43CY'
-sauce_options['name'] = 'bstackdemo-test'
+sauce_options['username'] = 'MND'
+sauce_options['accessKey'] = '83a9b712-fa08-44c5-9c69-2407e7f6b0bb'
+sauce_options['build'] = 'selenium-build-DJRHT'
+sauce_options['name'] = 'demo-test'
 options.set_capability('sauce:options', sauce_options)
 
 url = "https://ondemand.us-west-1.saucelabs.com:443/wd/hub"
@@ -27,6 +30,7 @@ driver = webdriver.Remote(command_executor=url, options=options)
 
 
 def click_to_signup():
+    print("Reach Sign up Page")
     # 虽然链接注册页但是会转入登录页
     driver.get('https://test-console.mnd.infothinker.com/signup/sendEmail')
     # 点击注册按钮跳转
@@ -47,6 +51,7 @@ def click_to_signup():
 
     
 def click_to_login():
+    print("Reach Login Page")
     driver.get('https://test-console.mnd.infothinker.com/login')
     WebDriverWait(driver, 10).until(
         EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/h1'), 'Welcome to Mainnet Digital, Sign in to Continue.')
@@ -65,7 +70,9 @@ def country_info_page():
         WebDriverWait(driver, 10).until(
             EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Country information')
         )
+        print("Reach Country info Page")
     except Exception as e:
+        print("Not Country info Page")
         pass
 
     # 上方左侧选框
@@ -93,7 +100,9 @@ def personal_info_page():
         WebDriverWait(driver, 10).until(
             EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Personal information')
         )
+        print("Reach Personal info Page")
     except Exception as e:
+        print("Not Personal info Page")
         pass   
     # 输入框
     input_box(
@@ -124,7 +133,9 @@ def residential_address_page():
         WebDriverWait(driver, 10).until(
             EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Residential address')
         )
+        print("Reach Residential address Page")
     except Exception as e:
+        print("Not Residential address Page")
         pass
 
     # 输入街道
@@ -155,7 +166,9 @@ def contact_info_page():
         WebDriverWait(driver, 10).until(
             EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Contact information')
         )
+        print("Reach Contact info Page")
     except Exception as e:
+        print("Not Contact info Page")
         pass
 
     # 输入国家代码
@@ -174,7 +187,9 @@ def investment_info_page():
         WebDriverWait(driver, 10).until(
             EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Investment information')
         )
+        print("Reach Investment information Page")
     except Exception as e:
+        print("Not Identification documents Page")
         pass
     # 选择交易种类
     box_select(driver, '//*[@id="root"]/div/div[2]/div[1]/div/div/div/div/div',
@@ -189,7 +204,9 @@ def asset_info_page():
         WebDriverWait(driver, 10).until(
             EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Asset information')
         )
+        print("Reach Asset information Page")
     except Exception as e:
+        print("Not Asset information Page")
         pass
 
     # 选择交易总额
@@ -214,25 +231,181 @@ def document_page():
         WebDriverWait(driver, 10).until(  
             EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Identification documents')
         )
+        print("Reach Identification documents Page")
     except Exception as e:
+        print("Not Identification documents Page")
         pass
 
-    # 左侧上传文件
-    upload_file(
-        driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div/div/span/div[1]/span/input',
-        'https://dropovercl.s3.amazonaws.com/985408bd-2262-490c-bb9d-9b965ba20193/9da2ca69-dcb7-4d94-a3ca-d9a40eba7c74/9708dd27-9215-406f-9a85-9044c9ca2207.png'
-    )
-    # 右侧上传文件
-    upload_file(
-        driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[3]/div/div/span/div[1]/span/input',
-        'https://dropovercl.s3.amazonaws.com/985408bd-2262-490c-bb9d-9b965ba20193/9da2ca69-dcb7-4d94-a3ca-d9a40eba7c74/9708dd27-9215-406f-9a85-9044c9ca2207.png'
-    )    
+    if platform.system() == "Darwin" :
+        # 左侧上传文件
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div/div/span/div[1]/span/input',
+            '/Users/chef/Downloads/Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div/div/span/div[2]/div/div'
+        )
+        # 右侧上传文件
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[3]/div/div/span/div[1]/span/input',
+            '/Users/chef/Downloads/Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div[3]/div/div/span/div[2]/div/div'
+        )     
+    elif platform.system() == "Windows" :
+        # 左侧上传文件
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div/div/span/div[1]/span/input',
+            'C:\\Users\\sauce\\Downloads\\Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div/div/span/div[2]/div/div'
+        )
+        # 右侧上传文件
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[3]/div/div/span/div[1]/span/input',
+            'C:\\Users\\sauce\\Downloads\\Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div[3]/div/div/span/div[2]/div/div'
+        )
+        pass
+    else :
+        # 左侧上传文件
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div/div/span/div[1]/span/input',
+            '/home/chef/Downloads/Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div/div/span/div[2]/div/div'
+        )
+        # 右侧上传文件
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div[3]/div/div/span/div[1]/span/input',
+            '/home/chef/Downloads/Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div[3]/div/div/span/div[2]/div/div'
+        )
     # next button
     button_click (driver, '//*[@id="root"]/div/div[2]/div[2]/button[2]')
-          
+
+
+def other_doc_page():
+    # 直到页面加载出来才开始操作
+    try:
+        WebDriverWait(driver, 10).until(  
+            EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Other documents')
+        )
+    except Exception as e:
+        print("Not Other documents Page")
+        pass
+
+    # 上传其他附件
+    if platform.system() == "Darwin" :
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div/div/div/span/div[1]/span/input',
+            '/Users/chef/Downloads/Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div/div/div/span/div[2]/div/div'
+        )
+    elif platform.system() == "Windows" :
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div/div/div/span/div[1]/span/input',
+            'C:\\Users\\sauce\\Downloads\\Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div/div/div/span/div[2]/div/div'
+        )
+    else :
+        upload_file(
+            driver, '//*[@id="root"]/div/div[2]/div[1]/div/div/div/div/span/div[1]/span/input',
+            '/home/chef/Downloads/Back.png',
+            '//*[@id="root"]/div/div[2]/div[1]/div/div/div/div/span/div[2]/div/div'
+        )
+    # next button
+    button_click(driver, '//*[@id="root"]/div/div[2]/div[2]/button[2]')
+
+
+def account_page():
+    # 直到页面加载出来才开始操作
+    try:
+        WebDriverWait(driver, 10).until(  
+            EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Account review')
+        )
+        print("Reach account Page")
+    except Exception as e:
+        print("Not account Page")
+        pass
+
+    input_box(
+        driver, '//*[@id="root"]/div/div[2]/div[1]/div/div/div[1]/textarea', '123456')
+    # next button
+    button_click(driver, '//*[@id="root"]/div/div[2]/div[2]/button[2]')
+    time.sleep(10)
+
+
+def disclaim_page():
+    # 直到页面加载出来才开始操作
+    try:
+        WebDriverWait(driver, 10).until(  
+            EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'MND disclaimer')
+        )
+        print("Reach disclaim Page")
+    except Exception as e:
+        print("Not disclaim Page")
+        pass
+    button_click(driver, '//*[@id="root"]/div/div[2]/div[1]/div[2]/div')
+    # next button
+    button_click(driver, '//*[@id="root"]/div/div[2]/div[2]/button[2]')
+    time.sleep(5)
+
+
+def agreement_page():
+    # 直到页面加载出来才开始操作
+    try:
+        WebDriverWait(driver, 10).until(  
+            EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'ACCREDITED INVESTOR OPT-IN APPLICATION')
+        )
+        print("Reach agreement Page")
+    except Exception as e:
+        print("Not agreement Page")
+        pass
+    
+    button_click(driver, '//*[@id="root"]/div/div[2]/div[1]/div[2]/div')
+    # next button
+    button_click(driver, '//*[@id="root"]/div/div[2]/div[2]/button[2]')
+    time.sleep(5)
+
+
+def face_detect_page():
+    # 直到页面加载出来才开始操作
+    try:
+        WebDriverWait(driver, 10).until(  
+            EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'Take photo of yourself with phone')
+        )
+        print("Reach face detect Page")
+    except Exception as e:
+        print("Not face detect Page")
+        pass
+    # next button
+    button_click(driver, '//*[@id="root"]/div/div[2]/div[2]/button[2]')
+    time.sleep(5)
+
+
+def general_page():
+    try:
+        WebDriverWait(driver, 10).until(  
+            EC.text_to_be_present_in_element((By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/h1'), 'General KYT')
+        )
+        print("Reach KYT Page")
+    except Exception as e:
+        print("Not KYT Page")
+        pass
+    # 选择币种
+    box_select(driver, '//*[@id="root"]/div/div[2]/div[1]/div[1]/div/div/div/div',
+               '/html/body/div[2]/div/div/div/div[2]/div[1]/div/div', 6)
+    # 输入货币地址
+    input_box(
+        driver, '//*[@id="root"]/div/div[2]/div[1]/div[2]/div/div[1]/input', '123456')
+    # next button
+    button_click(driver,'//*[@id="root"]/div/div[2]/div[2]/button[2]')
+
 
 def SignUp_Process_Control():
-    
+    # 下载测试文件
+    try :
+        download_file(driver, options, 'https://dropover.cloud/057635')
+    except Exception as e:
+        print("download fail")
+        pass
+          
     click_to_signup()
     click_to_login()
     country_info_page()
@@ -242,8 +415,15 @@ def SignUp_Process_Control():
     investment_info_page()
     asset_info_page()
     document_page()
-
+    other_doc_page()
+    account_page()
+    disclaim_page()
+    agreement_page()
+    face_detect_page()
+    general_page()
+    
     time.sleep(5)
+    print("Finished!")
     driver.quit()
 
 if __name__ == "__main__":
--- tests/components.py
+++ tests/components.py
@@ -5,8 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.keys import Keys
 from pykeyboard import PyKeyboard
 from pymouse import PyMouse
@@ -21,7 +19,11 @@ def button_click(driver, path):
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
             (By.XPATH, path))).click()
   except Exception as e:
-      pass
+    # 点击空白部分
+    button_click(driver, '//*[@id="root"]/div/div[1]/div/div[1]')
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, path))).click()
+    pass
 
 
 def input_box(driver,input_path, input_content):
@@ -29,16 +31,11 @@ def input_box(driver,input_path, input_content):
         By.XPATH, input_path).clear()
     time.sleep(1)
     # print(platform.system())
-    if platform.system() == "Darwin" :
-        driver.find_element(
-            By.XPATH, input_path).send_keys(Keys.CONTROL, 'a')
-        driver.find_element(
-            By.XPATH, input_path).send_keys(Keys.BACKSPACE)
-    else :
-        driver.find_element(
-        By.XPATH, input_path).send_keys(Keys.COMMAND, 'a')
+    while driver.find_element(
+            By.XPATH, input_path).get_attribute("value"):
         driver.find_element(
             By.XPATH, input_path).send_keys(Keys.BACKSPACE)
+
     driver.find_element(
         By.XPATH, input_path).send_keys(input_content)
 
@@ -69,11 +66,51 @@ def date_picker(driver,date_path, param):
     driver.find_element(By.XPATH, date_path).send_keys(param)
 
 
-def upload_file(driver, input_path, file_path):
+# 判断当前使用的是什么浏览器
+def get_browser_name(driver):
+    if isinstance(driver, webdriver.Chrome):
+        return "Chrome"
+    elif isinstance(driver, webdriver.Firefox):
+        return "Firefox"
+    elif isinstance(driver, webdriver.Edge):
+        return "Edge"
+    elif isinstance(driver, webdriver.Safari):
+        return "Safari"
+    elif isinstance(driver, webdriver.Ie):
+        return "Internet Explorer"
+    else:
+        return "Unknown"
+
+
+def download_file(driver, browser_option, url) :
+    # 获取当前文件上两级绝对路径
+    up_path = os.path.dirname(os.path.realpath(__file__))
+    up_up_path= os.path.dirname(up_path)
+    # 指定下载的默认文件夹, 区分Mac，win，linux系统
+    if platform.system() == "Darwin" :
+        prefs = {
+            "download.default_directory": up_up_path + '/assets',  # 下载存放目标文件夹
+            "download.prompt_for_download": False,  # 不提示下载对话框
+        }
+    elif platform.system() == "Windows":
+        prefs = {
+            "download.default_directory": up_up_path + "\\assets",  # 下载存放目标文件夹
+            "download.prompt_for_download": False,  # 不提示下载对话框
+        }
+    else :
+        pass
+    browser_option.add_experimental_option("prefs", prefs)
+    driver.get(url)
+    time.sleep(10)  # 等待下载完成
+
+
+def upload_file(driver, input_path, file_path, verify_path):
     # Ant Design是一个复杂组合组件，需要找到隐藏的基础组件
     try:
         file_input = driver.find_element(By.XPATH, input_path)
         file_input.send_keys(file_path)
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, verify_path)))
     except Exception as e:
         # 点击空白部分
+        print('upload fail')
         button_click(driver, '//*[@id="root"]/div/div[1]/div/div[1]')
